fb20765fc5ba470b5042fd77be8320b5322a2499
Amstel <dennis.brough@yandex.com>
2022-09-10 11:18:48 +0100

    housekeeping with tailwind incorporated

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,18 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "plugin:vue/vue3-recommended",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:vue/vue3-recommended", "eslint:recommended", "prettier"],
   parserOptions: {
     parser: "@babel/eslint-parser",
   },
+  plugins: ["vue", "prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
   overrides: [
     {
-      files: [
-        ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-      ],
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
       env: {
         jest: true,
       },
