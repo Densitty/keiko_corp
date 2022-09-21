eaea227205d24526ceacb9231fed5fb5436556e9
Amstel <dennis.brough@yandex.com>
2022-09-21 23:46:17 +0100

    laid out the layout of the hero section, as well as one of the hero components andcreated test for it

---
+++ src/components/navigation/Hero.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <section class="flex flex-col h-screen pt-10 pb-20 bg-orange-400">
+      <div class="grid grid-cols-12">
+        <div class="col-start-1 col-span-1 border-2 border-red-700">
+          this takes 1 column
+        </div>
+
+        <div class="col-start-2 col-span-5 border-2 border-red-700">
+          <headline></headline>
+        </div>
+
+        <div class="col-start-7 col-span-5 border-2 border-blue-700">
+          Doing this
+        </div>
+
+        <div class="col-start-12 col-span-1 border-2 border-blue-700">
+          Lesson
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+import Headline from "./hero_internals/Headline.vue";
+
+export default {
+  name: "Hero",
+  components: {
+    Headline,
+  },
+};
+</script>
+
+<style></style>
