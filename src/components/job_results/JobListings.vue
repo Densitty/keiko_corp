1627c1be26df98a9deedb09fde2983a80889ee19
Amstel <dennis.brough@yandex.com>
2022-09-27 01:41:30 +0100

    built the job listings for the jobs results page

---
+++ src/components/job_results/JobListings.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="flex-auto p-8 bg-gray-2">
+    <ul>
+      <job-listing></job-listing>
+      <job-listing></job-listing>
+      <job-listing></job-listing>
+      <job-listing></job-listing>
+      <job-listing></job-listing>
+    </ul>
+  </main>
+</template>
+
+<script>
+import JobListing from "./JobListing.vue";
+export default {
+  name: "JobListings",
+  components: {
+    JobListing,
+  },
+};
+</script>
+
+<style></style>
